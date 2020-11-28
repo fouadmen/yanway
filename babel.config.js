6e4d378087baa9521cd881594d6a1263fcafc89a
Fouad MANNOU <fouadmannou@Fouads-MacBook-Pro.local>
2020-11-28 13:01:11 +0900

    Project configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,35 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.js',
+          '.json',
+        ],
+        alias: {
+          "_assets": "./src/assets",
+          "_components": "./src/components",
+          "_atoms": "./src/components/atoms",
+          "_molecules": "./src/components/molecules",
+          "_organisms": "./src/components/organisms",
+          "_navigations": "./src/navigations",
+          "_scenes": "./src/scenes",
+          "_services": "./src/services",
+          "_styles": "./src/styles",
+          "_utils": "./src/utils",
+          "_reducers" : './src/redux_/reducers',
+          "_actions" : './src/redux_/actions',
+          "_redux" : "./src/redux_"
+        },
+      },
+    ]
+  ]
 };
