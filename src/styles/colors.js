1de0eaa85f85dad5f14b43bc423b747a2c6600a9
Fouad MANNOU <fouadmannou@Fouads-MacBook-Pro.local>
2020-12-05 12:28:00 +0900

    Change font

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+export const PRIMARY = '#03A9F4';
+export const SECONDARY = '#03A9F4';
+export const WHITE = '#FFFFFF';
+export const BLACK = '#454545';
+export const GOLD = '#FFC700';
+export const TEXT_BASIC_COLOR = "#054752";
+export const TEXT_HINT_COLOR = "#708C91";
+// ACTIONS
+export const SUCCESS = '#3adb76';
+export const WARNING = '#ffae00';
+export const ALERT = '#cc4b37';
+
+// GRAYSCALE
+export const GRAY_LIGHT = '#e6e6e6';
+export const GRAY_MEDIUM = '#cacaca';
+export const GRAY_DARK = '#8a8a8a';
---
+++ src/styles/typography.js
@@ -0,0 +1,20 @@
+import { scaleFont } from './mixins';
+
+// FONT FAMILY
+export const FONT_FAMILY_REGULAR = 'GTEesti-Regular';
+export const FONT_FAMILY_MEDIUM = 'GTEesti-Medium';
+export const FONT_FAMILY_BOLD = 'GTEesti-Bold';
+
+// FONT SIZE
+export const FONT_SIZE_32 = scaleFont(32);
+export const FONT_SIZE_28 = scaleFont(28);
+export const FONT_SIZE_24 = scaleFont(24);
+export const FONT_SIZE_20 = scaleFont(20);
+export const FONT_SIZE_18 = scaleFont(18);
+export const FONT_SIZE_16 = scaleFont(16);
+export const FONT_SIZE_14 = scaleFont(14);
+
+// LINE HEIGHT
+export const LINE_HEIGHT_24 = scaleFont(24);
+export const LINE_HEIGHT_20 = scaleFont(20);
+export const LINE_HEIGHT_16 = scaleFont(16);
